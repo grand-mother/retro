502ee6afe65ebc8041a4218fe3b745443d238698
Valentin Niess <niess@in2p3.fr>
2017-10-14 17:03:10 +0200

    Add a filter requiring tau vertices to be consistent with a conversion in the topography.

--- scripts/generate-tau-decays.py
+++ scripts/generate-tau-decays.py
@@ -1,8 +1,29 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  Copyright (C) 2017 Université Clermont Auvergne, CNRS/IN2P3, LPC
+#  Author: Valentin NIESS (niess@in2p3.fr)
+#
+#  Generate tau decay vertices for GRAND.
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>
+
 import json
 import math
 import sys
 import random
+# Custom import(s)
 from grand_tour import Topography
 
 def run(generator, logger, topography, comment=None):
@@ -27,7 +48,7 @@ def run(generator, logger, topography, comment=None):
         """Closure for generating a tentative tau direction before decay.
         """
         deg = math.pi / 180.
-        c1, c0 = (math.sin(x * deg) for x in generator["elevation"])
+        c0, c1 = (math.sin(x * deg) for x in generator["elevation"])
         weight = (c1 - c0) * 2. * math.pi
         def generate():
             c = random.uniform(c0, c1)
@@ -48,19 +69,37 @@ def run(generator, logger, topography, comment=None):
         return generate
     generate_energy = EnergyGenerator()
 
-    def EventLogger():
-        """Closure for logging events.
+    class EventLogger:
+        """Encapsulation for logging events.
         """
-        event = {}
-        path = logger["path"]
-        with open(path, "w+") as f: pass
-        def log(*args):
-            event["tau_at_decay"] = args
-            with open(path, "a") as f:
+        def __init__(self, path):
+            event = {}
+            self._path = path
+            with open(path, "w+") as f: pass
+            self._previous = -1
+
+        def __call__(self, *args):
+            event = { "previous" : self._previous, "tau_at_decay" : args }
+            with open(self._path, "a") as f:
+                self._previous = f.tell()
                 json.dump(event, f)
                 f.write("\n")
-        return log
-    log_event = EventLogger()
+    log_event = EventLogger(**logger)
+
+    def filter_event(energy, position, direction):
+        """Raw event filter from first principles.
+        """
+        # First let us compute the distance to the topography, propagating
+        # backwards.
+        dg = topo.distance(position, [-c for c in direction])
+        if dg is None: return 0.
+
+        # The let us compute the decay length, assuming no energy loss.
+        dl = energy * 4.89639E-05
+
+        # As selection weight, let us consider the probability that no decay
+        # occured on the path to rocks.
+        return math.exp(-dg / dl)
 
     # Main loop over events.
     trials = 0
@@ -72,9 +111,11 @@ def run(generator, logger, topography, comment=None):
         direction, w1 = generate_direction()
         energy, w2 = generate_energy()
 
-        # TODO: check if the generated direction is relevant considering the
+        # Check if the generated direction is relevant considering the
         # generated position and energy.
-        weight = w0 * w1 * w2
+        w3 = filter_event(energy, position, direction)
+        if (w3 <= 0.) or (random.random() > w3) : continue
+        weight = w0 * w1 * w2 / w3
 
         # TODO: decay the tau with danton, or alouette.
 
@@ -83,5 +124,6 @@ def run(generator, logger, topography, comment=None):
         trials = 0
 
 if __name__ == "__main__":
+    # Read the settings from a JSON card.
     with open(sys.argv[1]) as f: settings = json.load(f)
     run(**settings)
