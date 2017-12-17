f329d67e5130045846e3a48eb0db9450c6fd229e
Valentin Niess <niess@in2p3.fr>
2017-12-17 22:46:48 +0100

    Update the generation test for GRAND's convention

--- tests/test-generation.py
+++ tests/test-generation.py
@@ -13,8 +13,8 @@ topo = Topography(42.928056, 86.741667, "share/topography", 25)
 elevation = []
 for event in EventIterator("events-ulastai.json"):
     energy, position, direction = event["tau_at_decay"]
-    az, el = topo.local_to_horizontal(position, direction)
-    elevation.append(el)
+    theta, phi = topo.local_to_angular(position, direction)
+    elevation.append(90. - theta)
 
 p, x = numpy.histogram(elevation, 40, density=True)
 x = 0.5 * (x[1:] + x[:-1])
