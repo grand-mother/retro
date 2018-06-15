d46830216d76582686f85dc3eb41890ea1b13acd
Valentin Niess <niess@in2p3.fr>
2018-06-15 10:34:28 +0200

    Clean the build script and update TURTLE

--- build.sh
+++ build.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-#  Fetch dependencies"
-git submodule update --init
+# Fetch dependencies
+if [ -z "$(ls -A deps/danton)" ]; then
+    echo "Fetching dependencies..."
+    git submodule update --init
+fi
 
 # Bootstrap ninja
 if [ ! -e deps/ninja/ninja ]; then
-    cd deps/ninja ; ./configure.py --bootstrap ; cd ../..
+    cd deps/ninja
+    ./configure.py --bootstrap
+    cd ../..
 fi
 
 # Build the binaries"
 ./deps/ninja/ninja
-
